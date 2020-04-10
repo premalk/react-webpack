e79dc3d5b118c6ebbcaf6a51ca515d72c327b300
Premal Katigar <premalkatigar1992@gmail.com>
2020-04-10 13:07:34 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+module.exports = {  
+  module:{
+    rules:[
+      {
+          test:/\.(js|jsx)$/,
+          exclude:/node_modules/,
+          use: {
+              loader:'babel-loader',
+          }
+      },
+    ]
+  },
+  plugins: [
+    new HTMLWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+      }
+    )
+  ]
+};
